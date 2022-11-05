5de0d7f59fda51a2f4c3b040741e5332cce3dff1
Khaled-Walid <justkhwf@outlook.com>
2022-11-06 01:11:46 +0200

    improve TicketCard UI

--- src/components/TicketCard/TicketCard.tsx
+++ src/components/TicketCard/TicketCard.tsx
@@ -1,8 +1,10 @@
+import styles from './styles.module.css';
+
 export interface TicketCardData {
   _id: string;
   subject: string;
-  status: 'open' | 'closed' | 'in progress';
-  priority: 'low' | 'medium' | 'urgent';
+  status: 'open' | 'closed' | 'in progress' | 'status';
+  priority: 'low' | 'medium' | 'urgent' | 'priority';
   description: string;
 }
 
@@ -12,12 +14,40 @@ export interface TicketCardProps {
 
 export const TicketCard = ({ ticket }: TicketCardProps): JSX.Element => {
   const { _id, description, priority, status, subject } = ticket;
+  const statusColors = {
+    open: '#74c174',
+    'in progress': '#debd89',
+    closed: '#e66a6a',
+    status: '#000',
+  };
+  const priorityColors = {
+    low: '#008000',
+    medium: '#ce9337',
+    urgent: '#ff0000',
+    priority: '#000',
+  };
   return (
-    <>
-      <h3>{`Ticket #${_id}: ${subject}`}</h3>
-      <p>{description}</p>
-      <p>{status}</p>
-      <p>{priority}</p>
-    </>
+    <div
+      className={styles.container}
+      style={{
+        backgroundColor: _id === '#Ticket' ? '#cdd9f1' : 'inherit',
+      }}
+    >
+      <span className={styles.id}>{`${_id}`}</span>
+      <span className={styles.subject}>{`${subject}`}</span>
+      <span className={styles.description}>{description}</span>
+      <span
+        className={styles.status}
+        style={{ color: `${statusColors[status]}` }}
+      >
+        {status}
+      </span>
+      <span
+        className={styles.priority}
+        style={{ color: `${priorityColors[priority]}` }}
+      >
+        {priority}
+      </span>
+    </div>
   );
 };
