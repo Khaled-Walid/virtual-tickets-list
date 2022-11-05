a5c6b70374502d471481dff63e60a58356a9411c
Khaled-Walid <justkhwf@outlook.com>
2022-11-06 01:12:05 +0200

    add header to TicketsList

--- src/components/TicketsList/TicketsList.tsx
+++ src/components/TicketsList/TicketsList.tsx
@@ -11,13 +11,25 @@ export interface TicketsListProps {
 export const TicketsList = ({ data }: TicketsListProps): JSX.Element => {
   return (
     <div className={styles.ticketsList}>
+      <h1>All Tickets</h1>
+      <div className={styles.header}>
+        <TicketCard
+          ticket={{
+            _id: '#Ticket',
+            subject: 'Subject',
+            status: 'status',
+            priority: 'priority',
+            description: 'Description',
+          }}
+        />
+      </div>
       <VirtualList
         data={data}
         ItemComponent={TicketCard}
         SpinnerComponent={LoadingSpinner}
         idExtractor={(item: TicketCardProps) => item.ticket._id}
         listConfig={{
-          itemHeight: 350,
+          itemHeight: 80,
           numItems: data?.length ?? 0,
           windowHeight: 700,
         }}
